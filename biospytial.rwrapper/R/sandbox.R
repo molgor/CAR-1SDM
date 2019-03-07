f575eaa0c5b21578eb7b196068d6917be94a5654
Juan Escamilla <molgor@gmail.com>
2019-03-07 20:12:56 +0000

    Efficient debugging method for binomial.bymCAR.R

--- biospytial.rwrapper/R/sandbox.R
+++ biospytial.rwrapper/R/sandbox.R
@@ -2,13 +2,20 @@
 # i.e. it is a preprocessing script.
 # Change it appropriately
 
-my.binomial.bymCAR <- function(formula, data=NULL, trials, W, burnin, n.sample, thin=1, prior.mean.beta=NULL, prior.var.beta=NULL, prior.tau2=NULL, prior.sigma2=NULL, MALA=TRUE, verbose=TRUE)
+#my.binomial.bymCAR <- function(formula, data=NULL, trials, W, burnin, n.sample, thin=1, prior.mean.beta=NULL, prior.var.beta=NULL, prior.tau2=NULL, prior.sigma2=NULL, MALA=TRUE, verbose=TRUE)
 
 
-my.binomial.bymCAR(formula=formula_sample,W=M_bis,trials = trials,data=TDF,burnin=10000,n.sample=15000,verbose = TRUE)
+#my.binomial.bymCAR(formula=formula_sample,W=M_bis,trials = trials,data=TDF,burnin=10000,n.sample=15000,verbose = TRUE)
 
 
+#model.sample <-S.CARbym(formula=formula_sample,family="binomial",W=M_bis,trials = trials,data=TDF,burnin=10000,n.sample=15000,verbose = TRUE)
 
-    model.sample <-my_CARB(formula=formula_sample,family="binomial",W=M_bis,trials = trials,data=TDF,burnin=10000,n.sample=15000,verbose = TRUE,my_func=my.binomial.bymCAR)
+### Order of the code to be executed
+# 1. Load init_data
+# 2. Load imports.R
+# 3. Load as function my_CARbym in S.CARbym.R
+# 4. Load as function my.binomial.bymCAR in my.binomial.bymCAR.R
+# 5. Run the above line
 
-    model.sample <-S.CARbym(formula=formula_sample,family="binomial",W=M_bis,trials = trials,data=TDF,burnin=10000,n.sample=15000,verbose = TRUE)
+model.sample <-my_CARbym(formula=formula_sample,family="binomial",W=M_bis,trials = trials,data=TDF,burnin=10000,n.sample=15000,verbose = TRUE,custom_function=my.binomial.bymCAR) 
+
