abb22e3c3f1606eb09bebed5817ecc8a2ae58a69
Juan Escamilla Mólgora <juan@quercus.bioma.mx>
2021-09-08 17:44:46 -0500

    Changed name of repository
    The name of this repository has been changed.
    The old name was: biospytial_rwrapper
    
    Also, a new Readme.md file replaced the old one.
    
    Cheers
    
    Juan

---
+++ CAR-1SDM/R/init_data.R
@@ -0,0 +1,111 @@
+# This is a script for loading all the necessary data for processing 
+# i.e. it is a preprocessing script.
+# Change it appropriately
+
+#file = '/outputs/presence_only_models/predictors/dataset100x100-puebla-p9/0-pred.csv'
+#PDF = read.csv(file)
+## REad adjancency matrix
+
+## Load Stuff
+library(CARBayes)
+library(dplyr)
+library(purrr)
+library(reticulate)
+library(biospytial.rwrapper)
+
+## figure out a way to export the functions here. 
+# It works for presence_absence_naive. I dont know why not with the others
+source("SpeciesModels.R")
+# Import adjancency matrix generated from region
+mat_filename = "/outputs/training_data_sample_puebla_p9_abies_pinophyta_adjmat.npy"
+# Use numpy functions
+np <- import("numpy")
+M <- np$load(mat_filename)
+
+## Training data.frame
+TDF = read.csv("/outputs/training_data_sample_puebla_p9_abies_pinophyta.csv")
+#TDF = read.csv("/outputs/training_data_sample_puebla_p9_tyrannidae_birds.csv")
+## Order it according to the id of the cell
+### This is important because the adjancy matrix rows need to be the same
+TDF = TDF[order(TDF$cell_ids),]
+# Convert to numeric
+TDF = mutate_at(TDF,vars(Dist.to.road_m,Elevation_m,
+                         MaxTemp_m,MeanTemp_m,
+                         MinTemp_m,Population_m,
+                         Precipitation_m,
+                         SolarRadiation_m,
+                         VaporPres_m,
+                         WindSp_m),as.numeric)
+# Remove unnecessary symbols in variable names
+names(TDF) = lapply(names(TDF),function(x) gsub("_","",x))
+names(TDF) = lapply(names(TDF),function(x) gsub("\\.","",x))
+
+##### PREPROCESS
+###
+# Preprocess for generating pseudo absences
+# Change the name of a column that for some reason is called the same
+names(TDF)[23] <- 'covid2'
+
+
+## missing values only in X
+#DataFrame = TDF %>% rowwise() %>% 
+#            mutate(sample=pseudo_absence_naive(Plantae,LUCA),species=pseudo_absence_trivial(Pinophyta,Plantae))
+
+## missing values in X and Y
+DataFrame = TDF %>% rowwise() %>% 
+            mutate(sample=pseudo_absence_naive(Plantae,LUCA),species=pseudo_absence_naive(Pinophyta,Plantae))
+
+
+## Treatment for adding missing data species = Abies
+#DataFrame = TDF %>% rowwise() %>% 
+#            mutate(sample=pseudo_absence_naive(Pinophyta,Plantae),species=pseudo_absence_trivial(Abies,Pinophyta))
+
+
+############ This is for removing temporarily (for developing purposes) the NAN values.
+## remove nas
+#rr <- DataFrame %>% 
+#    filter(!is.na(species) & !is.na(sample))
+
+#sam_idx_nan <- which(is.na(DataFrame$sample))
+
+#M = M[-c(sam_idx_nan),-c(sam_idx_nan)]
+
+
+## this is for the moment not take the missing values
+
+#DataFrame = TDF %>% rowwise() %>% 
+#            mutate(sample=pseudo_absence_trivial(Plantae,LUCA),species=pseudo_absence_trivial(Pinophyta,Plantae))
+###
+#############
+
+
+## Remove entries with zero neighbours (adjancey matrix)
+### Calculates number of neighbours in D (sum)
+D = apply(M,MARGIN = 1,sum)
+### get index with 0 neighbours
+idx = which(D == 0)
+
+### select cells with no neighbours
+cell_with_no_neighbour = TDF$cellids[idx]
+## Remove island for TDF
+TDF <- TDF[-c(idx),]
+## Erase idx for M and for TDF (Or maybe only for M)
+M_bis = M[-c(idx),-c(idx)]
+
+## remove row corresponding to no neighbour (island)
+DataFrame <- DataFrame[-c(idx),]
+
+n <- dim(M_bis)[1]
+trials <- rep(1,n)
+
+## reformat dataframe
+#rr  <- rr[-c(idx),]
+
+# Formula definition
+formula_sample= sample~Disttoroadm+Populationm
+#+factor(tipos)
+#formula_presence= species~Elevationm+MeanTempm
+formula_presence= species~Elevationm+Precipitationm
+
+
+
--- README.md
+++ README.md
@@ -1,21 +1,13 @@
-# Biospytial Rwrapper 
-A wrapper to  communicate biospytial with R
+# Conditional autoregressive models for single species distributions using presence-only data
+This repository implements the presence-only models developed in the paper: 
+[[doi:https://doi.org/10.1101/2021.06.28.450233 ][A joint distribution framework to improve presence-only species distribution models by exploiting opportunistic surveys]]
 
-This is a packiage to use Dataframes in Biospytial compatible with the models used in Biospytial.
+The folder 'notebooks' contains the interactive pipelines for fitting and displaying models I, II and III. In the current version, the model fitting is performed in R with the package[[https://cran.r-project.org/web/packages/CARBayes/index.html][CARBayes]] (Lee, 2015) and the methods for displaying the results are done in Python. Therefore, it is recommended to use Jupyter notebooks with the R kernel installed for maximum compatibility.
 
-Currently supports CAR models. See documentation.
+This repository has two purposes. One, to create a reproducible environment for generating the results exposed in the paper (section: application). Second, to serve as a /user's guide/ for adapting the models to different types of problems. 
 
-### Development workflow
+As this repository is only for demonstrating purposes, it is not intended to be continuously developed in the long term. Must of the ideas specified in models I, II and III are already implemented in the package [[https://github.com/molgor/JSDM4POD][JSDM4POD]] (i.e. /joint species distribution models for presence-only data/). Currently: models I and II are contained in the JSDM4POD. In the coming future model III will be added.
 
-Please run the following
-1. imports.R (will import the necessary packages from CARBayes)
-2. init_data.R (will load the data and preprocess it)
+Thank you for your interest in this project. 
 
-3. my.binomial.bymCAR.R (this has the customized function) 
-
-
-### Current nightly version
-Run the sandox.R script to run model2
-
-
-
+Please consider sending your feedback to me: j.escamillamolgora[-at-]lancaster.ac.uk
